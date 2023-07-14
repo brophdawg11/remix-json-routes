1ea79bedf8512f5f2c19d3f7043a76d8af4938ba
Matt Brophy <matt@brophy.org>
2023-07-14 14:40:32 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Remix JSON Routes
+
+`remix-json-routes` is a package to allow you to define your Remix routes from a custom JSON structure, instead of (or in addition to ) the built in file-based routing convention.
+
+## Installation
+
+```sh
+npm install remix-json-routes
+```
+
+## Usage
+
+You leverage this package via the `routes` function in `remix.config.js`. The second argument to `jsonRoutes` is an array of routes similar to what you would pass to [`createBrowserRouter`](https://reactrouter.com/en/main/routers/create-browser-router) in React Router, where you define the route path information (`path`, `index`, `children`), but then instead of specifying an `element`/`action`/`loader`/etc., you specify the `file` path pointing to a Remix route file which exports those aspects.
+
+```js
+const { jsonRoutes } = require("remix-json-routes");
+
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  // Note this ignores everything in routes/ giving you complete control over
+  // your routes.  If you want to define routes in addition to what's in routes/,
+  // change this to "ignoredRouteFiles": ["**/*"].
+  ignoredRouteFiles: ["**/*"],
+  routes(defineRoutes) {
+    return jsonRoutes(defineRoutes, [
+      {
+        path: "/",
+        file: "routes/layout.tsx",
+        children: [
+          {
+            index: true,
+            file: "some/path/to/home.tsx",
+          },
+          {
+            path: "about",
+            file: "some/path/to/about.tsx",
+          },
+        ],
+      },
+    ]);
+  },
+};
+```
---
+++ index.js
@@ -0,0 +1,76 @@
+const React = require("react");
+
+function jsxRoutes(defineRoutes, routes) {
+  return jsonRoutes(defineRoutes, createRoutesFromElements(routes));
+}
+
+function jsonRoutes(defineRoutes, routes) {
+  return defineRoutes((route) => routes.forEach((r) => defineRoute(route, r)));
+}
+
+function defineRoute(routeFn, jsonRoute) {
+  let path = jsonRoute.path;
+  let file = jsonRoute.file;
+  let children = jsonRoute.children;
+  let opts = { ...jsonRoute };
+  delete opts.path;
+  delete opts.file;
+  if (children) {
+    routeFn(path, file, opts, () => {
+      children.forEach((c) => defineRoute(routeFn, c));
+    });
+  } else {
+    routeFn(path, file, opts);
+  }
+}
+
+function Route() {
+  throw new Error("<Route> is for defining routes, not for rendering");
+}
+
+function createRoutesFromElements(children, parentPath = []) {
+  let routes = [];
+
+  React.Children.forEach(children, (element, index) => {
+    let treePath = [...parentPath, index];
+
+    if (element.type === React.Fragment) {
+      routes.push(createRoutesFromElements(element.props.children, treePath));
+      return;
+    }
+
+    if (element.type !== Route) {
+      let type =
+        typeof element.type === "string" ? element.type : element.type.name;
+      throw new Error(
+        `Only <Route> elements are supported, found type: ${type}`
+      );
+    }
+
+    if (element.props.index && element.props.children) {
+      throw new Error("An index route cannot have child routes.");
+    }
+
+    let route = {
+      id: element.props.id || treePath.join("-"),
+      ...element.props,
+    };
+
+    if (element.props.children) {
+      route.children = createRoutesFromElements(
+        element.props.children,
+        treePath
+      );
+    }
+
+    routes.push(route);
+  });
+
+  return routes;
+}
+
+module.exports = {
+  jsonRoutes,
+  jsxRoutes,
+  Route,
+};
